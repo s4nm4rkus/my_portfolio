c4aaac6642055ff4d9e71193e9730049ae89cea1
s4nm4rkus <sanmarkusmorcoso@gmail.com>
2025-06-30 06:25:07 +0800

    done navs

--- src/components/Navs/homenav.jsx
+++ src/components/Navs/homenav.jsx
@@ -1,12 +1,16 @@
-import "./homenav.css";
 import { useState } from "react";
+import "./homenav.css";
+import mylogo from "../../assets/logo/my-logo.svg";
 
 function HomeNav() {
   const [isOpen, setIsOpen] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
   return (
     <nav className="nav-container">
-      <div className="nav-logo">MyPortfolio</div>
+      <div className="nav-logo">
+        <img src={mylogo} alt="Logo" className="mylogo" />
+      </div>
 
       <button className="nav-toggle" onClick={() => setIsOpen(!isOpen)}>
         ☰
@@ -17,7 +21,7 @@ function HomeNav() {
           <a href="/">Home</a>
         </li>
         <li>
-          <a href="#services">Servicest</a>
+          <a href="#services">Services</a>
         </li>
         <li>
           <a href="#projects">Projects</a>
@@ -26,7 +30,23 @@ function HomeNav() {
           <a href="#about">About</a>
         </li>
         <li>
-          <a href="#contact">Contact</a>
+          <a
+            href="#contact"
+            className="contact-btn"
+            style={{
+              color: hovered ? "white" : "white",
+              fontWeight: hovered ? "200" : "200",
+              transform: hovered ? "scale(1.03)" : "scale(1)",
+              boxShadow: hovered
+                ? "0 4px 12px rgba(138, 0, 0, 0.47)"
+                : "0 2px 6px rgba(0, 0, 0, 0.1)",
+              transition: "all 0.2s ease-in-out",
+            }}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          >
+            Contact
+          </a>
         </li>
       </ul>
     </nav>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,26 @@
 import "./home.css";
 import HomeNav from "../../components/Navs/homenav";
+import myImage from "../../assets/images/my-image.svg";
+import myImageOverlay from "../../assets/images/my-image-overlay.svg";
 
 function Home() {
   return (
     <>
       <HomeNav />
       <div className="home-wrapper">
-        <h1>Welcome to My Portfolio</h1>
-        <p>This is the homepage content.</p>
+        <div className="hero-container">
+          <div className="hero-text-container">
+            <p className="greet-text">Hello Im </p>
+            <span className="fullname-text">San Mark A. Morcoso</span>
+            <p className="name-text">Markus</p>
+            <p className="my-skill-text">
+              A Full-stack Developer and Front-end Designer
+            </p>
+          </div>
+          <div className="hero-images-container">
+            <img src="#" alt="My Image" className="myimage" />
+          </div>
+        </div>
       </div>
     </>
   );
